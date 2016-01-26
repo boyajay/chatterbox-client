9df38126175a9ca7152a0d56fdf65ea90f5dd2ad
boyajay <boyajay@github.com>
2016-01-25 17:07:26 -0800

    Implement dynamic drop down menu for different rooms.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,11 +8,13 @@ app.server = 'https://api.parse.com/1/classes/chatterbox?limit=1000';
 app.send = function(){
   //get the text from the input html field
   var msg = $('.input').val();
+  var usr = $('.name').val();
+  var rmname = $('.room').val()|| "public";
   console.log(msg, "msg");
   var msgObj = {
-    username: "BoyaANDHamzah",
+    username: usr,
     text: msg,
-    roomname: "public"
+    roomname: rmname
   };
   // console.log(msgObj, "msgObj!");
 
@@ -42,19 +44,38 @@ app.init = function(){};
 
 function getInfo(data){
   var dataHolder = data;
-  $("#chats").text(' ');
+  $('select').empty();
+  $("#chats").empty();
+  var rooms = [];
   for(var i = 0; i < dataHolder.results.length; i++) {
     var dataName = dataHolder.results[i].username;
     var dataText = dataHolder.results[i].text;
+    var currentroom = dataHolder.results[i].roomname;
+    // console.log("currentroom is "+ currentroom );
     var username;
     var text;
-
+    var roomName;
     if(dataName !== undefined){
      username = dataName.match(/[^\<\>\(\)\'\"\{\}\&\|\[\]]+/ig);
      }
-     if(dataText !== undefined){
+    if(dataText !== undefined){
      text = dataText.match(/[^\<\>\(\)\'\"\{\}\&\|\[\]]+/ig);
      }
     $("#chats").prepend( username + ": " + text + '<br>');
+    
+
+    if(currentroom !== undefined){
+     roomName= currentroom.match(/[^\<\>\(\)\'\"\{\}\&\|\[\]]+/ig);
+    
+     }
+    
+    if (roomName && rooms.indexOf(roomName[0]) === -1){
+      rooms.push(roomName[0]);
+     
+    }
+  }
+  for(var j = 0; j < rooms.length; j++) {
+    $('select').append("<option value="+ rooms[j] + ">" + rooms[j] + "</option>");
   }
 }
+

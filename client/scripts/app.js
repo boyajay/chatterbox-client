88a63a4a2e23c0f69e88464d9c83a74aba470019
boyajay <boyajay@github.com>
2016-01-25 16:12:07 -0800

    Implement message posting and refresh feed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,48 @@
 // YOUR CODE HERE:
 var app ={};
-app.server = 'https://api.parse.com/1/classes/chatterbox'; 
+app.server = 'https://api.parse.com/1/classes/chatterbox?limit=1000'; 
 
+
+
+//$('.submit').on('click', app.send);
 app.send = function(){
+  //get the text from the input html field
+  var msg = $('.input').val();
+  console.log(msg, "msg");
+  var msgObj = {
+    username: "BoyaANDHamzah",
+    text: msg,
+    roomname: "public"
+  };
+  // console.log(msgObj, "msgObj!");
 
-
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(msgObj),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+  test();
 };
+
+function test (){
+  console.log(app.server);
+  $.get(app.server, getInfo);
+}
+
 $.get(app.server, getInfo);
 
 app.init = function(){};
 
 function getInfo(data){
   var dataHolder = data;
+  $("#chats").text(' ');
   for(var i = 0; i < dataHolder.results.length; i++) {
     var dataName = dataHolder.results[i].username;
     var dataText = dataHolder.results[i].text;
@@ -27,25 +58,3 @@ function getInfo(data){
     $("#chats").append( username + ": " + text + '<br>');
   }
 }
-
-
-// $.ajax({
-//   // This is the url you should use to communicate with the parse API server.
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'POST',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent');
-//   },
-//   error: function (data) {
-//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message');
-//   }
-// });
-
-// var message = {
-//   username: 'shawndrost',
-//   text: 'trololo',
-//   roomname: '4chan'
-// };
